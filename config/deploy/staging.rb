6099bffe86ede4de5be31ba50e7fe30b456af3dc
Nathan Batson <nathanbatson@nathan-batson-2.local>
2010-11-04 16:26:19 -0400

    updated the deploy for examples

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,19 +1,20 @@
 ################################################################################################################
-# This deploy recipe will deploy a project from a github repo to a VPS server
+# This deploy recipe will deploy a project from a Codaset repo to a Linode VPS server
 #
 # Assumptions:
-#   * You are using VPS hosting with SSH access
+#   * You are using Linode for hosting, but this would most likely work on any VPS, such as Slicehost
 #   * Your deployment directory is located in /srv/www/
+#   * This is a Rails project and will use the staging environment
 #
 #################################################################################################################
 #
 # Change this to the name of the project.  It should match the name of the Git repo.
 # This will set the name of the project directory and become the subdomain
-set :project, 'your-project' # the GitHub project name
-set :github_user, "your-username" # Your GitHub username
-set :domain_name, "your-project.your-staging-server" # should be something like project.mydomain.com
-set :user, 'ssh-username' # VPS hosting ssh username
-set :domain, 'mydomain.com' # IP or domain name to ssh to
+set :project, 'merge-development-test' 
+set :github_user, "nathanbatson" # Your GitHub username
+set :domain_name, "mergetraining.comicalconcept.com" # should be something like mydomain.com
+set :user, 'nathanbatson' # Linode username
+set :domain, '74.207.233.153' # Linode IP address
 
 #### You shouldn't need to change anything below ########################################################
 default_run_options[:pty] = true
@@ -21,7 +22,7 @@ default_run_options[:pty] = true
 set :repository,  "git@github.com:#{github_user}/#{project}.git" #GitHub clone URL
 set :scm, "git"
 set :scm_passphrase, "" # This is the passphrase for the ssh key on the server deployed to
-set :branch, "master"   # This is for a git branch if you want to deploy from something besides master
+set :branch, "master"
 set :scm_verbose, true
 set :applicationdir, "/srv/www/#{domain_name}"
 set :keep_releases, 1
@@ -30,6 +31,7 @@ set :keep_releases, 1
 # deploy config
 set :deploy_to, applicationdir
 set :deploy_via, :remote_cache
+set :runner, "nathanbatson" 
  
 # roles (servers)
 role :app, domain
@@ -48,10 +50,24 @@ namespace :deploy do
   end
 end
 
+namespace :crontab do
+  desc "Update the crontab based on config/schedule.rb"
+  task :update, :roles => :app, :except => { :no_release => true } do
+    run "crontab -r"
+    run "cd #{current_path} && rake update_crontab RAILS_ENV=production"
+  end
+end
+
+# additional settings
+#default_run_options[:pty] = true  # Forgo errors when deploying from windows
+#ssh_options[:keys] = %w(/Path/To/id_rsa)            # If you are using ssh_keys
+
 set :use_sudo, false
  
 # Optional tasks ##########################################################################################
 # for use with shared files (e.g. config files)
 after "deploy:update_code" do
-  run "ln -s #{shared_path}/public/directory-of-images #{release_path}/public"
+  # run "ln -s #{shared_path}/print_files #{release_path}"
+  # run "ln -s #{shared_path}/illustration #{release_path}/public"
+  # run "ln -s #{shared_path}/ads #{release_path}/public"
 end
